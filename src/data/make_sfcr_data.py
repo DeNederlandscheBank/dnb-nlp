fa8b617a6d27d70d7b5dd6eba0cc8f4c83ed6094
wjwillemse <w.j.willemse@xs4all.nl>
2019-11-12 14:04:59 +0100

    html scraping added

--- src/data/make_sfcr_data.py
+++ src/data/make_sfcr_data.py
@@ -11,6 +11,9 @@ import re
 import requests
 from src.nlp.text_extraction import doc2text
 
+from bs4 import BeautifulSoup as soup
+import urllib
+
 METADATA_PATH = join('data', 'external')
 EXTERNAL_PATH = join('data', 'external', 'sfcr')
 INTERIM_PATH = join('data', 'interim', 'sfcr')
@@ -26,11 +29,7 @@ def main(output_path, interim_path):
     logger = logging.getLogger(__name__)
     logger.info('Downloading SFCRs')
 
-    df_sfcr = pd.read_csv(join(METADATA_PATH, 'metadata_sfcr.csv'))
-
-    df_sfcr['Filename'] = ""
-
-    def download(filename, url, output_path):
+    def download_pdf(filename, url, output_path):
         if not(isfile(join(output_path, filename))):
             logger.info('--Retrieving %s' % filename)
             r = requests.get(url)
@@ -39,32 +38,70 @@ def main(output_path, interim_path):
             f.close()
         else:
             logger.info('--SFCR %s already downloaded' % filename)
+        return 0
+
+    def download_html(filename, url, output_path):
+        html = requests.get(url)
+        if html.status_code==200:
+            bs = soup(html.text, features="html.parser")
+            links = bs.findAll('a')
+            n_pdfs = 0
+            for link in links:
+                link_string = str(link).lower()
+                if (("sfcr" in link_string) or 
+                    ("solvency and financial condition" in link_string)) and (".pdf" in link_string):
+                    content = requests.get(urllib.parse.urljoin(url, link['href']), output_path)
+                    if content.status_code==200 and content.headers['content-type']=='application/pdf':
+                        n_pdfs += 1
+                        with open(join(output_path, filename[:-4]+"_"+str(n_pdfs)+'.pdf'), 'wb') as pdf:
+                            pdf.write(content.content)
+        logger.info("--{0} pdfs downloaded and saved in {1}".format(n_pdfs, output_path))        
+        return n_pdfs
+
+    def convert_to_text(filename, output_path, interim_path):
+        logger.info('Converting %s to txt' % pdf_filename)
+        logger.info('--Doc2text')
+        logging.getLogger().setLevel(logging.ERROR)
+        text = doc2text(join(output_path, pdf_filename))
+        logging.getLogger().setLevel(logging.INFO)
+        logger.info('--Saving to txt file')
+        txt = open(join(interim_path, txt_filename), "wb")
+        txt.write(text.encode('utf-8'))
+        txt.close()
+
+    df_sfcr = pd.read_csv(join(METADATA_PATH, 'metadata_sfcr.csv'))
+
+    df_sfcr['Filename'] = ""
+    df_sfcr['Number of pdfs'] = 0
 
     for row in df_sfcr.index:
         name = df_sfcr.loc[row, 'Insurance Undertaking']
         document = df_sfcr.loc[row, 'Document Type']
         url = df_sfcr.loc[row, 'Url']
+        url_type = df_sfcr.loc[row, 'Url Type']
         filename = name + "_" + document + '.pdf'
         df_sfcr.loc[row, "Filename"] = filename
-        download(filename, url, output_path)
+        if url_type=="PDF":
+            n_pdfs = download_pdf(filename, url, output_path)
+        else:
+            n_pdfs = download_html(filename, url, output_path)
+        df_sfcr.loc[row, "Number of pdfs"] = n_pdfs
 
     files = [f for f in listdir(interim_path) if isfile(join(interim_path, f))]    
 
     for row in df_sfcr.index:
 
-        pdf_filename = df_sfcr.loc[row, "Filename"]
-        txt_filename = df_sfcr.loc[row, "Filename"][:-3] + 'txt'
-
-        if txt_filename not in files:
-            logger.info('Converting %s to txt' % pdf_filename)
-            logger.info('--Doc2text')
-            logging.getLogger().setLevel(logging.ERROR)
-            text = doc2text(join(output_path, pdf_filename))
-            logging.getLogger().setLevel(logging.INFO)
-            logger.info('--Saving to txt file')
-            txt = open(join(interim_path, txt_filename), "wb")
-            txt.write(text.encode('utf-8'))
-            txt.close()
+        if df_sfcr.loc[row, "Url Type"]=="PDF":
+            pdf_filename = df_sfcr.loc[row, "Filename"]
+            txt_filename = df_sfcr.loc[row, "Filename"][:-3] + 'txt'
+            if txt_filename not in files:
+                convert_to_text(filename, output_path, interim_path)
+        else:
+            for n in range(df_sfcr.loc[row, "Number of pdfs"]):
+                pdf_filename = df_sfcr.loc[row, "Filename"][:-4] + "_"+str(n+1) + ".pdf"
+                txt_filename = pdf_filename[:-3]+'txt'
+                if txt_filename not in files:
+                    convert_to_text(filename, output_path, interim_path)
     
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
