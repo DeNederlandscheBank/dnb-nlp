58052e338bf809998eac9b6d893d6124705701c3
wjwillemse <w.j.willemse@xs4all.nl>
2020-01-28 13:20:20 +0100

    updates

--- src/utils/make_dataset.py
+++ src/utils/make_dataset.py
@@ -11,12 +11,22 @@ from src.utils.text_extraction import doc2text, doc2dataframe
  
 EXTERNAL_PATH = join('data', 'external', 'sfcr')
 INTERIM_PATH = join('data', 'interim', 'sfcr')
- 
+FILE_EXTENSION = "pdf"
+FILE_TERMS = ""
+LANGUAGE = "en"
+DOCUMENT_TYPE = "unknown"
+DOCUMENT_YEAR = 0
+
 @click.command()
 @click.option('--output_path', default=EXTERNAL_PATH, help='The path of the downloaded files.')
 @click.option('--interim_path', default=INTERIM_PATH, help='The path of the interim files.')
- 
-def main(output_path, interim_path):
+@click.option('--file_extension', default=FILE_EXTENSION, help='The file extension to select.')
+@click.option('--file_terms', default=FILE_TERMS, help='The file terms to selects.')
+@click.option('--language', default=LANGUAGE, help='The language of the files.')
+@click.option('--document_type', default=DOCUMENT_TYPE, help='The document type of the files.')
+@click.option('--document_year', default=DOCUMENT_YEAR, help='The document year of the files.')
+
+def main(output_path, interim_path, file_extension, file_terms, language, document_type, document_year):
     """Reads all pdfs in external_filepath and converts to txt and dataframes in interim_path
     """
     logger = logging.getLogger(__name__)
@@ -25,28 +35,30 @@ def main(output_path, interim_path):
     logger.info('Writing in %s' % interim_path)
     external_files = []
     for dirpath, dirnames, filenames in walk(output_path):
-        for filename in [f for f in filenames if f.endswith(".pdf")]:
-            external_files.append((dirpath, filename))
+        for filename in [f for f in filenames if f.endswith("." + file_extension)]:
+            if file_terms.lower() in filename.lower():
+                external_files.append((dirpath, filename))
  
     for file in external_files:
         logger.info('Processing %s' % file[1])
         pdf_filename = file[1]
-        txt_filename = file[1][:-3] + 'txt'
-        pck_filename = file[1][:-3] + 'pickle'
+        txt_filename = file[1][:-len(file_extension)] + 'txt'
+        pck_filename = file[1][:-len(file_extension)] + 'pickle'
         new_dir = join(interim_path, file[0][len(output_path):])
         if not exists(new_dir):
+            logger.info('Making new directory %s' % interim_path)
             makedirs(new_dir)
         if not isfile(join(new_dir, txt_filename)):
-            convert_to_text(pdf_filename, txt_filename, file[0], new_dir)
+            convert_to_text(pdf_filename, txt_filename, file[0], new_dir, language)
         if not isfile(join(new_dir, pck_filename)):
-            convert_to_dataframe(pdf_filename, pck_filename, file[0], new_dir)
+            convert_to_dataframe(pdf_filename, pck_filename, file[0], new_dir, language, document_type, document_year)
  
-def convert_to_text(pdf_filename, txt_filename, output_path, interim_path):
+def convert_to_text(pdf_filename, txt_filename, output_path, interim_path, language):
     logger = logging.getLogger(__name__)
     logger.info('Converting %s to txt' % pdf_filename)
     logger.info('--Doc2text')
     logging.getLogger().setLevel(logging.ERROR)
-    text = doc2text(join(output_path, pdf_filename))
+    text = doc2text(join(output_path, pdf_filename), language)
     logging.getLogger().setLevel(logging.INFO)
     logger.info('--Saving to txt file')
     txt = open(join(interim_path, txt_filename), "wb")
@@ -54,12 +66,12 @@ def convert_to_text(pdf_filename, txt_filename, output_path, interim_path):
     txt.write(text.encode('utf-8'))
     txt.close()
  
-def convert_to_dataframe(pdf_filename, pck_filename, output_path, interim_path):
+def convert_to_dataframe(pdf_filename, pck_filename, output_path, interim_path, language, document_type, document_year):
     logger = logging.getLogger(__name__)
     logger.info('Converting %s to dataframe' % pdf_filename)
     logger.info('--Doc2DataFrame')
     logging.getLogger().setLevel(logging.ERROR)
-    df = doc2dataframe(join(output_path, pdf_filename))
+    df = doc2dataframe(join(output_path, pdf_filename), language, document_type, document_year)
     logging.getLogger().setLevel(logging.INFO)
     logger.info('--Saving to dataframe file')
     logger.info('--Writing file %s' % str(join(interim_path, pck_filename)))
--- src/utils/text_extraction.py
+++ src/utils/text_extraction.py
@@ -14,6 +14,9 @@ from pdfminer.pdfparser import PDFParser
 from lexnlp.nlp.en.segments import sentences as sentences_en
 from src.nlp.nl.segments import sentences as sentences_nl
 
+import nltk
+from bs4 import BeautifulSoup
+
 def doc2text(path, language='en'):
     """
     Simple doc2text method.
@@ -23,43 +26,58 @@ def doc2text(path, language='en'):
     """
     codec = 'utf-8'
     output_string = StringIO()
-    with open(path, 'rb') as in_file:
-        parser = PDFParser(in_file)
-        doc = PDFDocument(parser)
-        rsrcmgr = PDFResourceManager()
-        device = TextConverter(rsrcmgr, output_string, codec = codec, laparams=LAParams())
-        interpreter = PDFPageInterpreter(rsrcmgr, device)
-        for page in PDFPage.create_pages(doc):
-            interpreter.process_page(page)
-    text = output_string.getvalue()
-    return text
-
-def doc2dataframe(path, lang=None):
-    """
-    Convert pdf document to dataframe (each sentence separately)
-    """
-
-    if ("_nl_" in path.lower()) or (lang == "nl") or ("//nl//" in path.lower()):
-        sentences = sentences_nl
-        lang = "nl"
-    else:
-        sentences = sentences_en
-        lang = "en"
-
-    df = pd.DataFrame(columns = ['source', 'lang', 'page', 'sentence', 'text'])
-    codec = 'utf-8'
-    with open(path, 'rb') as in_file:
-        parser = PDFParser(in_file)
-        doc = PDFDocument(parser)
-        for page_idx, page in enumerate(PDFPage.create_pages(doc)):
-            output_string = StringIO()
+    if path[-3:].lower()=='pdf':
+        with open(path, 'rb') as in_file:
+            parser = PDFParser(in_file)
+            doc = PDFDocument(parser)
             rsrcmgr = PDFResourceManager()
             device = TextConverter(rsrcmgr, output_string, codec = codec, laparams=LAParams())
             interpreter = PDFPageInterpreter(rsrcmgr, device)
-            interpreter.process_page(page)
-            text = output_string.getvalue()
+            for page in PDFPage.create_pages(doc):
+                interpreter.process_page(page)
+        text = output_string.getvalue()
+    elif path[-4:].lower()=='html':
+        with open(path, 'rb') as in_file:
+            soup = BeautifulSoup(in_file)
+            text = soup.get_text()
+    return text
+
+def doc2dataframe(path, language=None, document_type=None, document_year=None):
+    """
+    Convert pdf document to dataframe (each sentence separately)
+    """
+    if language is not None:
+        if ("_nl_" in path.lower()) or (language == "nl") or ("//nl//" in path.lower()):
+            sentences = sentences_nl
+            language = "nl"
+        else:
+            sentences = sentences_en
+            language = "en"
+
+    df = pd.DataFrame(columns = ['source', 'language', 'document type', 'document year', 'page', 'sentence', 'text'])
+    codec = 'utf-8'
+    if path[-3:].lower()=='pdf':
+        with open(path, 'rb') as in_file:
+            parser = PDFParser(in_file)
+            doc = PDFDocument(parser)
+            for page_idx, page in enumerate(PDFPage.create_pages(doc)):
+                output_string = StringIO()
+                rsrcmgr = PDFResourceManager()
+                device = TextConverter(rsrcmgr, output_string, codec = codec, laparams=LAParams())
+                interpreter = PDFPageInterpreter(rsrcmgr, device)
+                interpreter.process_page(page)
+                text = output_string.getvalue()
+                text = sentences.pre_process_document(text)
+                for sentence_idx, sentence in enumerate(sentences.get_sentence_list(text)):
+                    df = df.append(pd.DataFrame(columns = ['source','language', 'document type', 'document year', 'page', 'sentence', 'text'], 
+                                                data = [[path, language, document_type, document_year, page_idx, sentence_idx, sentence]]), ignore_index=True)
+    elif path[-4:].lower()=='html':
+        with open(path, 'rb') as in_file:
+            soup = BeautifulSoup(in_file)
+            text = soup.get_text()
             text = sentences.pre_process_document(text)
             for sentence_idx, sentence in enumerate(sentences.get_sentence_list(text)):
-                df = df.append(pd.DataFrame(columns = ['source','lang', 'page', 'sentence', 'text'], 
-                                            data = [[path, lang, page_idx, sentence_idx, sentence]]), ignore_index=True)
+                    df = df.append(pd.DataFrame(columns = ['source','language', 'document type', 'document year', 'page', 'sentence', 'text'], 
+                                                data = [[path, language, document_type, document_year, 0, sentence_idx, sentence]]), ignore_index=True)
+
     return df
